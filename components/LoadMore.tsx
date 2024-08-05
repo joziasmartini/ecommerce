596c3551d0775ca3549b1c5df46f20b46db7956e
Jozias Martini <jozias.martini@gmail.com>
2024-08-05 16:55:27 -0300

    style: improvements in design

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -5,7 +5,7 @@ export default function LoadMore({}: LoadMoreProps) {
     <div className="flex flex-col items-center">
       <button
         type="button"
-        className="my-20 rounded-full bg-gray-100 px-20 py-2.5 text-sm font-semibold text-orange-600 shadow-sm hover:bg-orange-200 transition-all duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="my-20 rounded-full hover:bg-orange-600 px-20 py-2.5 text-sm font-semibold hover:text-white shadow-sm bg-orange-200 text-orange-600 transition-all duration-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         Carregar outros
       </button>
